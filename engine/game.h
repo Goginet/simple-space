878625eea24e0810bf31d3c0fb853a12054c3dca
Goginet <gogi.soft.gm@gmail.com>
2016-03-30 09:30:36 +0300

    add Box2D SFML move game.(h/cpp) update makefile

---
+++ engine/game.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "GSM/include/gsm.h"
+#include "../SFML/Graphics.hpp"
+
+class Game
+{
+  public:
+    Game(State *startState,float width, float hight);
+    ~Game();
+    void start();
+    void end();
+    void restart();
+    void back();
+  private:
+    // delta время для отрисовки и обновления логики
+    const static unsigned int DELTA_TIME = 1;
+    bool alive;
+    // указатель на окно игры
+    sf::RenderWindow *window;
+    // менеджер состояний
+    GSM *gsm;
+    static void render(GSM *gsm);
+    static void update(GSM *gsm, unsigned int deltaTime);
+    void control();
+    //std::thread renderThread;                               // TODO: многопоточность отложим до лучших времен)
+    //std::thread updateThread;
+    //std::thread control;
+};
